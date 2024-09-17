73fab6c6dee236631d5e33c1d557f597ae573ec1
muby97 <mubarakfolorunsho@gmail.com>
2024-09-17 18:44:11 +0100

    Update users.py

--- modules/users.py
+++ modules/users.py
@@ -1,19 +1,20 @@
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
+from modules.database import get_db_connection
 
-DATABASE_FILE = r"C:\Users\User\Documents\FinalMSCProject\database\CARBON_FOOTPRINT.db"
+# DATABASE_FILE = r"C:\Users\User\Documents\FinalMSCProject\database\CARBON_FOOTPRINT.db"
 
 def register_user(username, password):
     # Use 'pbkdf2:sha256' instead of 'sha256'
     password = generate_password_hash(password, method='pbkdf2:sha256')
-    with sqlite3.connect(DATABASE_FILE) as conn:
+    with get_db_connection() as conn:
         cursor = conn.cursor()
         cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
         conn.commit()
 
 
 def login_user(username, password):
-    with sqlite3.connect(DATABASE_FILE) as conn:
+    with get_db_connection() as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT password FROM users WHERE username = ?", (username,))
         result = cursor.fetchone()
